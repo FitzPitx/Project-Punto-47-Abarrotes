0853247cd9d4e052a6527d98eba9d60b7ed02af2
Andrés Mosquera <85971409+andresmosqueraw@users.noreply.github.com>
2023-09-22 19:31:15 -0500

    Configuracion ips

--- app/src/main/java/consulting/gigs/api/ValuesAPI.java
+++ app/src/main/java/consulting/gigs/api/ValuesAPI.java
@@ -6,5 +6,11 @@ public abstract interface ValuesAPI {
   // String BASE_URL = "http://192.168.1.8/apiGigs/";
 
  // IP Universidad Mosquera
- String BASE_URL = "http://10.57.30.208/apiAbarrotes47/";
+ //String BASE_URL = "http://10.57.30.208/apiAbarrotes47/";
+
+ // IP Casa Mosquera
+ // String BASE_URL = "http://192.168.1.4/apiAbarrotes47/";
+
+ // IP iphone Mosquera
+ String BASE_URL = "http://172.20.10.3/apiAbarrotes47/";
 }
