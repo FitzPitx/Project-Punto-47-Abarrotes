8e14c383a74a9a366f7908e092f6d2965724654d
FitzPitx <anieto@uniempresarial.edu.co>
2023-09-25 09:47:04 -0500

    add changes

--- app/src/main/java/consulting/gigs/api/ValuesAPI.java
+++ app/src/main/java/consulting/gigs/api/ValuesAPI.java
@@ -3,8 +3,8 @@ package consulting.gigs.api;
 public abstract interface ValuesAPI {
 
  //String BASE_URL = "http://192.168.20.59/apiGigs/";
-  // String BASE_URL = "http://192.168.1.8/apiGigs/";
+ //String BASE_URL = "http://192.168.1.8/apiGigs/";
 
  // IP Universidad Mosquera
- String BASE_URL = "http://10.57.30.208/apiAbarrotes47/";
+ String BASE_URL = "http://192.168.1.8/apiAbarrotes47/";
 }
---
+++ db/usuario.sql
@@ -0,0 +1,65 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 23, 2023 at 01:21 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `abarrotes_47`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `id_usuario` int(11) NOT NULL,
+  `nombre_usuario` varchar(255) NOT NULL,
+  `apellido_usuario` varchar(255) NOT NULL,
+  `correo_usuario` varchar(255) NOT NULL,
+  `username_usuario` varchar(255) NOT NULL,
+  `password_usuario` varchar(255) NOT NULL,
+  `identifier_usuario` varchar(255) NOT NULL,
+  `key_usuario` varchar(255) NOT NULL,
+  `estatus_usuario` int(1) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id_usuario`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
