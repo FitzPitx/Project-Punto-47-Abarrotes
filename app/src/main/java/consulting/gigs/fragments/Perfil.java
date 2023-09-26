f9020c178d29013557df6218f7f0f63b154aa857
FitzPitx <anieto@uniempresarial.edu.co>
2023-09-26 15:07:27 -0500

    delete apiFilesOld

--- app/src/main/java/consulting/gigs/fragments/Perfil.java
+++ app/src/main/java/consulting/gigs/fragments/Perfil.java
@@ -2,7 +2,6 @@ package consulting.gigs.fragments;
 
 import static android.util.Base64.NO_WRAP;
 import static consulting.gigs.loginActivity.isNullOrEmpty;
-import static consulting.gigs.api.ValuesAPI.BASE_URL;
 
 import android.content.Context;
 import android.content.SharedPreferences;
@@ -23,10 +22,6 @@ import android.widget.Toast;
 import java.util.ArrayList;
 
 import consulting.gigs.R;
-import consulting.gigs.api.ServiceVisit;
-import consulting.gigs.model.visita.ResponseVisit;
-import consulting.gigs.model.visita.Visit;
-import consulting.gigs.api.ClienteRetrofit;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -91,56 +86,13 @@ public class Perfil extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_perfil, container, false);
-
-        frInit(view);
-        getCredencials(view);
         return view;
     }
 
-    private void listaEventos(String identificador,String key,String id, View view){
-        String token = identificador+":"+key;
-        final String AUTH = "Basic "+ Base64.encodeToString((token).getBytes(), NO_WRAP);
-        retrofit = ClienteRetrofit.getClient(BASE_URL,AUTH);
-        ServiceVisit serviceVisit = retrofit.create(ServiceVisit.class);
-        Call<ResponseVisit> call = serviceVisit.visitList(id,AUTH);
-        call.enqueue(new Callback<ResponseVisit>() {
-            @Override
-            public void onResponse(Call<ResponseVisit> call, Response<ResponseVisit> response) {
-                Toast.makeText(getActivity(), "COdigo:"+response.code(), Toast.LENGTH_SHORT).show();
-                Log.i("RAPTOR1", "onResponse: "+response.body().getMensaje());
-                if (response.isSuccessful()){
-                    ResponseVisit body = response.body();
-                    String mensaje = body.getMensaje();
-                    ArrayList<Visit> list = body.getVisita();
 
-                    if(mensaje.equals("OK") && !isNullOrEmpty(list)){
-                        ArrayAdapter<Visit> adapter = new ArrayAdapter<>
-                                (getActivity().getApplicationContext(), android.R.layout.simple_list_item_1,list);
-                        lista.setAdapter(adapter);
-                    }
-                }else{
-                    Toast.makeText(getActivity(), "NO error", Toast.LENGTH_SHORT).show();
-                    Log.i("RAPTOR2", "onResponse: "+response.body());
-                }
-            }
 
-            @Override
-            public void onFailure(Call<ResponseVisit> call, Throwable t) {
-                Log.i("RAPTOR3", "onFailure: "+t.getMessage());
-            }
-        });
-    }
-
-    private void frInit(View view){
+    private void frInit(View view) {
         lista = view.findViewById(R.id.lvEventos);
 
-    }
-    private void getCredencials(View view){
-        SharedPreferences preferences = getActivity().getSharedPreferences("credenciales", Context.MODE_PRIVATE);
-        String key = preferences.getString("key","No");
-        String identificador = preferences.getString("identificador","No");
-        String id = preferences.getString("id","NoID");
-        Toast.makeText(getActivity(), ""+id+identificador+key, Toast.LENGTH_SHORT).show();
-        listaEventos(identificador,key,id, view);
     }
 }
